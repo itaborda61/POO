d12e0083e9fb8cf4e58ff4562f46fddc7c78b825
Ivan Taborda <ivantaborda.61@gmail.com>
2024-08-27 05:37:34 -0500

    Primer commit

---
+++ ClassProducto.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class Producto{
+    public $strDescripcion;
+    public $fltPrecio;
+    protected $intStockMinimo =10;
+    protected $strStatus ="Activo";
+
+public function __construct(string $descripcion, float $precio)
+{
+    $this->strDescripcion =$descripcion;
+    $this->fltPrecio = $precio;
+}
+public 
+
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+?>
